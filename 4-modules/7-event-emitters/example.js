7b1faf89b3bd2bacfdd84b30c5ea15497ac4e8ee
VukasinVujic <vukasinvujic021@gmail.com>
2020-01-13 17:36:28 +0100

    learning about emmiters

--- 4-modules/7-event-emitters/example.js
+++ 4-modules/7-event-emitters/example.js
@@ -1,4 +1,25 @@
 const EventEmitter = require('events');
 
+const myEmitter = new EventEmitter();
+
+setImmediate( () => {
+    myEmitter.emit('TEST_EVENT');
+});
+
+myEmitter.on('TEST_EVENT' , () => {
+    console.log('test event was fired');
+});
+
+myEmitter.on('TEST_EVENT' , () => {
+    console.log('test event was fired');
+});
+
+myEmitter.on('TEST_EVENT' , () => {
+    console.log('test event was fired');
+});
+
+// myEmitter.emit('TEST_EVENT');
+
+
 // Streams are Event Emitters
 // process.stdin, process.stdout
