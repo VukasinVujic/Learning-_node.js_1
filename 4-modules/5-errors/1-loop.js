8c24a779ebac56cada7c47c52363ae81026593b1
VukasinVujic <vukasinvujic021@gmail.com>
2020-01-13 12:48:07 +0100

    learning about errors

--- 4-modules/5-errors/1-loop.js
+++ 4-modules/5-errors/1-loop.js
@@ -4,7 +4,11 @@ const fs = require('fs');
 const files = ['.bash_profile', '.npmrc'];
 
 files.forEach(file => {
+  try {
   const filePath = path.resolve(process.env.HOME, file);
   const data = fs.readFileSync(filePath);
   console.log('File data is', data);
+  }catch(error){
+    console.log('file not find');
+  }
 });
