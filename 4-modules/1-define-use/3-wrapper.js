64f594e65ec81ec42d0f74655fb938cbadedf74f
VukasinVujic <vukasinvujic021@gmail.com>
2020-01-13 11:35:43 +0100

    learning about wrapper function

--- 4-modules/1-define-use/3-wrapper.js
+++ 4-modules/1-define-use/3-wrapper.js
@@ -1,1 +1,17 @@
+// function (exports, module, require, __filename, __dirname) {
+
+// let g = 1; 
+
 console.log(arguments);
+
+exports.a = 41;
+
+module.exports.b = 54;
+
+// exports = () => {}; NOT OK
+
+module.exports = () => {}; // OK
+
+// return module.exports;
+
+// } (module.exports,)
