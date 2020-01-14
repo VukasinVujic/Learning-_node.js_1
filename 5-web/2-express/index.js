3e3dceb8f6391a8cd01a27819ce70f259e685436
VukasinVujic <vukasinvujic021@gmail.com>
2020-01-14 10:50:55 +0100

    basic from express

--- 5-web/2-express/index.js
+++ 5-web/2-express/index.js
@@ -2,6 +2,14 @@ const express = require('express');
 
 const server = express();
 
+server.get('/' , (reg, res) => {
+   res.send('Hello expres from Vule  ');
+})
+server.get('/about' , (reg, res) => {
+  res.send('About...');
+})
+
+
 server.listen(4242, () => {
-  console.log('Express Server is running...');
+  console.log('Express Server is running NOW...');
 });
